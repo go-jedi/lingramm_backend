a1da3e881b363450a15147fed65f4be2e080d44b
go-jedi <golangjedi@gmail.com>
2025-05-03 17:38:04 +0300

    add get user by uuid repository and add tests

---
+++ internal/repository/user/get_by_uuid/get_by_uuid.go
@@ -0,0 +1,71 @@
+package getbyuuid
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"time"
+
+	"github.com/go-jedi/lingvogramm_backend/internal/domain/user"
+	"github.com/go-jedi/lingvogramm_backend/pkg/logger"
+	"github.com/go-jedi/lingvogramm_backend/pkg/postgres"
+	"github.com/jackc/pgx/v5"
+)
+
+//go:generate mockery --name=IGetByUUID --output=mocks --case=underscore
+type IGetByUUID interface {
+	Execute(ctx context.Context, tx pgx.Tx, uuid string) (user.User, error)
+}
+
+type GetByUUID struct {
+	queryTimeout int64
+	logger       logger.ILogger
+}
+
+func New(logger logger.ILogger) *GetByUUID {
+	gbui := &GetByUUID{
+		logger: logger,
+	}
+
+	gbui.init()
+
+	return gbui
+}
+
+func (gbui *GetByUUID) init() {
+	if gbui.queryTimeout == 0 {
+		gbui.queryTimeout = postgres.DefaultQueryTimeout
+	}
+}
+
+func (gbui *GetByUUID) Execute(ctx context.Context, tx pgx.Tx, uuid string) (user.User, error) {
+	gbui.logger.Debug("[get user by uuid] execute repository")
+
+	ctxTimeout, cancel := context.WithTimeout(ctx, time.Duration(gbui.queryTimeout)*time.Second)
+	defer cancel()
+
+	q := `
+		SELECT *
+		FROM users
+		WHERE uuid = $1;
+	`
+
+	var u user.User
+
+	if err := tx.QueryRow(
+		ctxTimeout, q, uuid,
+	).Scan(
+		&u.ID, &u.UUID, &u.TelegramID,
+		&u.Username, &u.FirstName, &u.LastName,
+		&u.CreatedAt, &u.UpdatedAt,
+	); err != nil {
+		if errors.Is(err, context.DeadlineExceeded) {
+			gbui.logger.Error("request timed out while get user by uuid", "err", err)
+			return user.User{}, fmt.Errorf("the request timed out: %w", err)
+		}
+		gbui.logger.Error("failed to get user by uuid", "err", err)
+		return user.User{}, fmt.Errorf("could not get user by uuid: %w", err)
+	}
+
+	return u, nil
+}
--- internal/repository/user/repository.go
+++ internal/repository/user/repository.go
@@ -4,6 +4,7 @@ import (
 	"github.com/go-jedi/lingvogramm_backend/internal/repository/user/create"
 	"github.com/go-jedi/lingvogramm_backend/internal/repository/user/exists"
 	getbytelegramid "github.com/go-jedi/lingvogramm_backend/internal/repository/user/get_by_telegram_id"
+	getbyuuid "github.com/go-jedi/lingvogramm_backend/internal/repository/user/get_by_uuid"
 	"github.com/go-jedi/lingvogramm_backend/pkg/logger"
 )
 
@@ -11,6 +12,7 @@ type Repository struct {
 	Create          create.ICreate
 	Exists          exists.IExists
 	GetByTelegramID getbytelegramid.IGetByTelegramID
+	GetByUUID       getbyuuid.IGetByUUID
 }
 
 func New(
@@ -20,5 +22,6 @@ func New(
 		Create:          create.New(logger),
 		Exists:          exists.New(logger),
 		GetByTelegramID: getbytelegramid.New(logger),
+		GetByUUID:       getbyuuid.New(logger),
 	}
 }
