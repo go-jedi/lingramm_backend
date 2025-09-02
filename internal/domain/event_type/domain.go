36e9b53f6c87b482bfa52a2ea8dbeffd665a70f0
go-jedi <golangjedi@gmail.com>
2025-09-02 16:34:15 +0300

    add swagger information about handlers event type

--- internal/domain/event_type/domain.go
+++ internal/domain/event_type/domain.go
@@ -32,3 +32,68 @@ type CreateDTO struct {
 	IsSendNotification  bool             `json:"is_send_notification"`
 	IsActive            bool             `json:"is_active"`
 }
+
+//
+// SWAGGER
+//
+
+type CreateSwaggerResponse struct {
+	Status  bool   `json:"status" example:"true"`
+	Message string `json:"message" example:"success"`
+	Error   string `json:"error" example:""`
+	Data    struct {
+		ID                  int64     `json:"id" example:"1"`
+		Name                string    `json:"name" example:"some name"`
+		Description         *string   `json:"description,omitempty" example:"some description"`
+		XP                  int64     `json:"xp" example:"20"`
+		Amount              *int64    `json:"amount,omitempty" example:"10"`
+		NotificationMessage *string   `json:"notification_message,omitempty" example:"some message"`
+		IsSendNotification  bool      `json:"is_send_notification" example:"true"`
+		IsActive            bool      `json:"is_active" example:"true"`
+		CreatedAt           time.Time `json:"created_at" example:"2025-09-02T12:48:06.37622+03:00"`
+		UpdatedAt           time.Time `json:"updated_at" example:"2025-09-02T12:48:06.37622+03:00"`
+	} `json:"data"`
+}
+
+type AllSwaggerResponse struct {
+	Status  bool   `json:"status" example:"true"`
+	Message string `json:"message" example:"success"`
+	Error   string `json:"error" example:""`
+	Data    []struct {
+		ID                  int64     `json:"id" example:"1"`
+		Name                string    `json:"name" example:"some name"`
+		Description         *string   `json:"description,omitempty" example:"some description"`
+		XP                  int64     `json:"xp" example:"20"`
+		Amount              *int64    `json:"amount,omitempty" example:"10"`
+		NotificationMessage *string   `json:"notification_message,omitempty" example:"some message"`
+		IsSendNotification  bool      `json:"is_send_notification" example:"true"`
+		IsActive            bool      `json:"is_active" example:"true"`
+		CreatedAt           time.Time `json:"created_at" example:"2025-09-02T12:48:06.37622+03:00"`
+		UpdatedAt           time.Time `json:"updated_at" example:"2025-09-02T12:48:06.37622+03:00"`
+	} `json:"data"`
+}
+
+type GetByNameSwaggerResponse struct {
+	Status  bool   `json:"status" example:"true"`
+	Message string `json:"message" example:"success"`
+	Error   string `json:"error" example:""`
+	Data    []struct {
+		ID                  int64     `json:"id" example:"1"`
+		Name                string    `json:"name" example:"some name"`
+		Description         *string   `json:"description,omitempty" example:"some description"`
+		XP                  int64     `json:"xp" example:"20"`
+		Amount              *int64    `json:"amount,omitempty" example:"10"`
+		NotificationMessage *string   `json:"notification_message,omitempty" example:"some message"`
+		IsSendNotification  bool      `json:"is_send_notification" example:"true"`
+		IsActive            bool      `json:"is_active" example:"true"`
+		CreatedAt           time.Time `json:"created_at" example:"2025-09-02T12:48:06.37622+03:00"`
+		UpdatedAt           time.Time `json:"updated_at" example:"2025-09-02T12:48:06.37622+03:00"`
+	} `json:"data"`
+}
+
+type ErrorSwaggerResponse struct {
+	Status  bool        `json:"status" example:"false"`
+	Message string      `json:"message" example:"some error"`
+	Error   string      `json:"error" example:"some error"`
+	Data    interface{} `json:"data"`
+}
